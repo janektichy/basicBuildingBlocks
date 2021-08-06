b095523ad89c3d4bf8d5e311bfc1cfc0c5e80efd
janektichy <janek.tichy@email.cz>
2021-08-07 01:05:25 +0200

    start

---
+++ java.js
@@ -0,0 +1,33 @@
+for (let i = 0; i < 10; i++) {
+console.log(i)
+} 
+
+for (let i = 1; i < 11; i++) {
+    console.log(i)
+} 
+    
+for (let i = 2; i < 21; i = i + 2) {
+    console.log(i)
+} 
+
+let something = (a, b) => {
+    return a + b;
+}
+
+console.log(something(5, 2))
+        
+
+function input(a) {
+    for (let i = 1; i < a; i++) {
+        console.log(i)
+    } 
+}
+
+input(20);
+
+function greet(name) {
+    console.log("Hello " + name);
+}
+
+greet('John');
+
